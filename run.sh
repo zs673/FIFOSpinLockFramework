596b4d0b4d34ac36b07dbf1ffac161783a0014ab
Z Shuai <zs673@research2.york.ac.uk>
2017-05-06 01:58:32 +0100

    master

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ rm -rf *.txt
 cd ..
 ls
 
-nohup java -cp bin/ test.GATestParallel
+javac src/analysis/*.java src/entity/*.java src/geneticAlgoritmSolver/*.java src/basicAnalysis/*.java src/generatorTools/*.java src/test/*.java
+
+nohup java -cp src/ test.GATestParallel
