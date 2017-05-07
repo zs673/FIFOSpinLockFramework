78c3ff481e58f702a745d1868172cc0e96ac6e9e
Z Shuai <zs673@research2.york.ac.uk>
2017-05-07 17:27:39 +0100

    master

--- run.sh
+++ run.sh
@@ -4,17 +4,6 @@ rm -rf *.txt
 cd ..
 ls
 
-javac src/noAllocation/analysis/*.java 
-src/noAllocation/entity/*.java \
-src/noAllocation/geneticAlgoritmSolver/*.java \
-src/noAllocation/basicAnalysis/*.java \
-src/noAllocation/generatorTools/*.java \
-src/noAllocation/test/*.java \
-src/allocation/analysis/*.java \
-src/allocation/entity/*.java \
-src/allocation/geneticAlgoritmSolver/*.java \
-src/allocation/basicAnalysis/*.java \
-src/allocation/generatorTools/*.java \
-src/allocation/test/*.java
+javac $(find ./src/* | grep .java)
 
 nohup java -cp src/ noAllocation.test.GATestParallel
