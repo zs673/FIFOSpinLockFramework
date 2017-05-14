7e7c003d1c35d46f0720cf315c6feaf56cc85099
shuai <shuai@Shuai>
2017-05-14 11:40:05 +0100

    XXX

--- src/test/StaticTest3cslen.java
+++ src/test/StaticTest3cslen.java
@@ -19,7 +19,7 @@ import generatorTools.GeneatorUtils.RESOURCES_RANGE;
 import generatorTools.SystemGenerator;
 
 public class StaticTest3cslen {
-	public static int MAX_PERIOD = 20;
+	public static int MAX_PERIOD = 50;
 	public static int MIN_PERIOD = 10;
 	static int NUMBER_OF_MAX_ACCESS_TO_ONE_RESOURCE = 2;
 	static int NUMBER_OF_TASKS_ON_EACH_PARTITION = 4;
