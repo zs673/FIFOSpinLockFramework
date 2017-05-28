ba74763d75eac984e78fdc3fc90b7f567e59b9c9
shuai <shuai@Shuai>
2017-05-28 16:02:09 +0100

    xxx

--- src/evaluation/DynamicTestWithAllocation.java
+++ src/evaluation/DynamicTestWithAllocation.java
@@ -38,29 +38,29 @@ public class DynamicTestWithAllocation {
 	public static void main(String[] args) throws Exception {
 		DynamicTestWithAllocation test = new DynamicTestWithAllocation();
 
-		// final CountDownLatch cslencountdown = new CountDownLatch(6);
-		// for (int i = 1; i < 7; i++) {
-		// final int cslen = i;
-		// new Thread(new Runnable() {
-		// @Override
-		// public void run() {
-		// test.experimentIncreasingCriticalSectionLength(cslen);
-		// cslencountdown.countDown();
-		// }
-		// }).start();
-		// }
-		//
-		// final CountDownLatch workloadcountdown = new CountDownLatch(9);
-		// for (int i = 1; i < 10; i++) {
-		// final int workload = i;
-		// new Thread(new Runnable() {
-		// @Override
-		// public void run() {
-		// test.experimentIncreasingWorkLoad(workload);
-		// workloadcountdown.countDown();
-		// }
-		// }).start();
-		// }
+		final CountDownLatch cslencountdown = new CountDownLatch(6);
+		for (int i = 1; i < 7; i++) {
+			final int cslen = i;
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					test.experimentIncreasingCriticalSectionLength(cslen);
+					cslencountdown.countDown();
+				}
+			}).start();
+		}
+
+		final CountDownLatch workloadcountdown = new CountDownLatch(9);
+		for (int i = 1; i < 10; i++) {
+			final int workload = i;
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					test.experimentIncreasingWorkLoad(workload);
+					workloadcountdown.countDown();
+				}
+			}).start();
+		}
 
 		final CountDownLatch accesscountdown = new CountDownLatch(5);
 		for (int i = 1; i < 22; i = i + 5) {
@@ -74,22 +74,22 @@ public class DynamicTestWithAllocation {
 			}).start();
 		}
 
-		// final CountDownLatch processorscountdown = new CountDownLatch(8);
-		// for (int i = 2; i < 17; i = i + 2) {
-		// final int processors = i;
-		// new Thread(new Runnable() {
-		// @Override
-		// public void run() {
-		// test.experimentIncreasingParallel(processors);
-		// processorscountdown.countDown();
-		// }
-		// }).start();
-		// }
+		final CountDownLatch processorscountdown = new CountDownLatch(8);
+		for (int i = 2; i < 17; i = i + 2) {
+			final int processors = i;
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					test.experimentIncreasingParallel(processors);
+					processorscountdown.countDown();
+				}
+			}).start();
+		}
 
-		// cslencountdown.await();
-		// workloadcountdown.await();
+		cslencountdown.await();
+		workloadcountdown.await();
 		accesscountdown.await();
-		// processorscountdown.await();
+		processorscountdown.await();
 		IOAResultReader.schedreader(null, false);
 	}
 
