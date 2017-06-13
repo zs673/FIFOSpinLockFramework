dcfe0075d4fa12f9b3c4f90eb860bdcada424041
shuai <shuai@Shuai>
2017-06-13 04:28:33 +0100

    xxx

--- src/generatorTools/SystemGeneratorWithAllocation.java
+++ src/generatorTools/SystemGeneratorWithAllocation.java
@@ -779,57 +779,56 @@ public class SystemGeneratorWithAllocation {
 
 		return FirstFitAllocation(sortedT, partitions);
 	}
-	
 
 	public ArrayList<ArrayList<SporadicTask>> assignPrioritiesByDM(ArrayList<ArrayList<SporadicTask>> tasksToAssgin,
 			ArrayList<Resource> resources) {
-		if (tasksToAssgin == null || resources == null) {
-			System.out.println("priority assigment error!, tasks or resources is null");
-			System.exit(-1);
+		if (tasksToAssgin == null) {
+			return null;
 		}
 
 		ArrayList<ArrayList<SporadicTask>> tasks = new ArrayList<>(tasksToAssgin);
-
 		// ASSIGN PRIORITIES
 		for (int i = 0; i < tasks.size(); i++) {
 			new PriorityGeneator().deadlineMonotonicPriorityAssignment(tasks.get(i), tasks.get(i).size());
 		}
 
-		for (int i = 0; i < resources.size(); i++) {
-			Resource res = resources.get(i);
-			res.ceiling.clear();
-			res.isGlobal = false;
-			res.partitions.clear();
-			res.requested_tasks.clear();
-		}
-
-		/* for each resource */
-		for (int i = 0; i < resources.size(); i++) {
-			Resource resource = resources.get(i);
-
-			/* for each partition */
-			for (int j = 0; j < tasks.size(); j++) {
-				int ceiling = 0;
-
-				/* for each task in the given partition */
-				for (int k = 0; k < tasks.get(j).size(); k++) {
-					SporadicTask task = tasks.get(j).get(k);
-
-					if (task.resource_required_index.contains(resource.id - 1)) {
-						resource.requested_tasks.add(task);
-						ceiling = task.priority > ceiling ? task.priority : ceiling;
-						if (!resource.partitions.contains(task.partition)) {
-							resource.partitions.add(task.partition);
-						}
-					}
-				}
-
-				if (ceiling > 0)
-					resource.ceiling.add(ceiling);
+		if (resources != null && resources.size() > 0) {
+			for (int i = 0; i < resources.size(); i++) {
+				Resource res = resources.get(i);
+				res.ceiling.clear();
+				res.isGlobal = false;
+				res.partitions.clear();
+				res.requested_tasks.clear();
 			}
 
-			if (resource.partitions.size() > 1)
-				resource.isGlobal = true;
+			/* for each resource */
+			for (int i = 0; i < resources.size(); i++) {
+				Resource resource = resources.get(i);
+
+				/* for each partition */
+				for (int j = 0; j < tasks.size(); j++) {
+					int ceiling = 0;
+
+					/* for each task in the given partition */
+					for (int k = 0; k < tasks.get(j).size(); k++) {
+						SporadicTask task = tasks.get(j).get(k);
+
+						if (task.resource_required_index.contains(resource.id - 1)) {
+							resource.requested_tasks.add(task);
+							ceiling = task.priority > ceiling ? task.priority : ceiling;
+							if (!resource.partitions.contains(task.partition)) {
+								resource.partitions.add(task.partition);
+							}
+						}
+					}
+
+					if (ceiling > 0)
+						resource.ceiling.add(ceiling);
+				}
+
+				if (resource.partitions.size() > 1)
+					resource.isGlobal = true;
+			}
 		}
 
 		return tasks;
