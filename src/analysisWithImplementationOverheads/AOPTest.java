a5d243ffcc5c4e28ec2cdff53775d8b2615ec038
shuai <shuai@Shuai>
2017-06-29 10:18:25 +0100

    xxx

--- src/analysisWithImplementationOverheads/AOPTest.java
+++ src/analysisWithImplementationOverheads/AOPTest.java
@@ -7,11 +7,14 @@ import entity.SporadicTask;
 
 public class AOPTest {
 
-	
-	public static boolean testSchedulable(ArrayList<ArrayList<SporadicTask>> tasks, ArrayList<Resource> resources){
+	public static boolean testSchedulable(ArrayList<ArrayList<SporadicTask>> tasks, ArrayList<Resource> resources) {
 		boolean isSchedulable = true;
-		
-		
+
+		ArrayList<Integer> priority_levels = new ArrayList<>();
+		for (int i = 0; i < tasks.size(); i++) {
+			priority_levels.add(tasks.get(i).size());
+		}
+
 		return isSchedulable;
 	}
 }
