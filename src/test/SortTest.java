1639104f70e107302d18ca2bfcaa66325b6d6331
zs673 <zs673@york.ac.uk>
2017-07-03 11:27:11 +0100

    xxx

---
+++ src/test/SortTest.java
@@ -0,0 +1,25 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class SortTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> array = new ArrayList<>();
+		
+		for(int i=0;i<10;i++){
+			array.add(new Random().nextInt(10000));
+		}
+		
+		array.sort((p1, p2) -> Double.compare(p1, p2));
+		System.out.println(Arrays.toString(array.toArray()));
+		
+		array.sort((p1, p2) -> Double.compare(p2, p1));
+		System.out.println(Arrays.toString(array.toArray()));
+		
+	}
+
+}
