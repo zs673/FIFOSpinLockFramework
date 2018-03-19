cc41b377742551ba8fffb83b0541465b26a65bfd
zhaos <zhaos@DESKTOP-AKJHLMR>
2018-03-19 03:21:47 +0000

    x

---
+++ src/evaluationSection6/PossibleTest.java
@@ -0,0 +1,131 @@
+package evaluationSection6;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+
+import GeneticAlgorithmFramework.PreGASolver;
+import entity.Resource;
+import entity.SporadicTask;
+import generatorTools.SystemGenerator;
+import utils.GeneatorUtils.CS_LENGTH_RANGE;
+import utils.GeneatorUtils.RESOURCES_RANGE;
+
+public class PossibleTest {
+
+	public static int TOTAL_NUMBER_OF_SYSTEMS = 100;
+	public static int MIN_PERIOD = 1;
+	public static int MAX_PERIOD = 1000;
+	public static int TOTAL_PARTITIONS = 16;
+
+	int NUMBER_OF_TASKS_ON_EACH_PARTITION = 4;
+	final double RSF = 0.3;
+
+	public static void main(String[] args) throws InterruptedException {
+		PossibleTest possible = new PossibleTest();
+		CountDownLatch down = new CountDownLatch(12);
+		for (int i = 1; i < 7; i++) {
+			final int cslen = i;
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					possible.test(cslen, 3);
+					down.countDown();
+				}
+			}).start();
+		}
+		
+		for (int i = 1; i < 27; i+=5) {
+			final int access = i;
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					possible.test(6, access);
+					down.countDown();
+				}
+			}).start();
+		}
+		
+		down.await();
+	}
+
+	public void test(int cslen, int NoA) {
+		int possible = 0, impossible = 0;
+		final CS_LENGTH_RANGE cs_range;
+		switch (cslen) {
+		case 1:
+			cs_range = CS_LENGTH_RANGE.VERY_SHORT_CS_LEN;
+			break;
+		case 2:
+			cs_range = CS_LENGTH_RANGE.SHORT_CS_LEN;
+			break;
+		case 3:
+			cs_range = CS_LENGTH_RANGE.MEDIUM_CS_LEN;
+			break;
+		case 4:
+			cs_range = CS_LENGTH_RANGE.LONG_CSLEN;
+			break;
+		case 5:
+			cs_range = CS_LENGTH_RANGE.VERY_LONG_CSLEN;
+			break;
+		case 6:
+			cs_range = CS_LENGTH_RANGE.RANDOM;
+			break;
+		default:
+			cs_range = null;
+			break;
+		}
+
+		for (int i = 0; i < TOTAL_NUMBER_OF_SYSTEMS; i++) {
+			SystemGenerator generator = new SystemGenerator(MIN_PERIOD, MAX_PERIOD, true, TOTAL_PARTITIONS,
+					TOTAL_PARTITIONS * NUMBER_OF_TASKS_ON_EACH_PARTITION, RSF, cs_range, RESOURCES_RANGE.PARTITIONS, NoA, false);
+
+			ArrayList<SporadicTask> tasksToAlloc = generator.generateTasks();
+			ArrayList<Resource> resources = generator.generateResources();
+			generator.generateResourceUsage(tasksToAlloc, resources);
+			PreGASolver pre = new PreGASolver(tasksToAlloc, resources, generator, 3, 1, 1, false);
+
+			int preres = pre.initialCheck(true);
+			while (preres == 1) {
+				tasksToAlloc = generator.generateTasks();
+				resources = generator.generateResources();
+				generator.generateResourceUsage(tasksToAlloc, resources);
+
+				pre = new PreGASolver(tasksToAlloc, resources, generator, 3, 1, 1, false);
+				preres = pre.initialCheck(true);
+			}
+
+			if (preres == 0)
+				possible++;
+			if (preres == -1)
+				impossible++;
+			System.out.println("cslen : " + cslen + " access: " + NoA + " count: " + i);
+		}
+		System.out.println("cslen : " + cslen + " access: " + NoA + "   possible: " + possible + "  impossible: " + impossible);
+		String result = "cslen : " + cslen + " access: " + NoA + "   possible: " + possible + "  impossible: " + impossible;
+		writeSystem("cslen" + cslen + "access" + NoA, result);
+	}
+
+	public void writeSystem(String filename, String result) {
+		PrintWriter writer = null;
+
+		try {
+			writer = new PrintWriter(new FileWriter(new File("result/" + filename + ".txt"), false));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		writer.println(result);
+		writer.close();
+	}
+
+}
--- src/evaluationSection6/ProtocolsCombinedWFDMSuccessF.java
+++ src/evaluationSection6/ProtocolsCombinedWFDMSuccessF.java
@@ -11,23 +11,15 @@ import java.util.concurrent.CountDownLatch;
 
 import GeneticAlgorithmFramework.GASolver;
 import GeneticAlgorithmFramework.PreGASolver;
-import analysis.FIFONP;
-import analysis.FIFOP;
-import analysis.MrsP;
 import entity.Resource;
 import entity.SporadicTask;
-import generatorTools.AllocationGeneator;
 import generatorTools.SystemGenerator;
-import utils.AnalysisUtils;
 import utils.GeneatorUtils.CS_LENGTH_RANGE;
 import utils.GeneatorUtils.RESOURCES_RANGE;
-import utils.ResultReader;
 
 public class ProtocolsCombinedWFDMSuccessF {
 
-	public static int TOTAL_NUMBER_OF_SYSTEMS = 100;
-	public static int NUMBER_OF_TIMES = 30;
-
+	public static int TOTAL_NUMBER_OF_SYSTEMS = 1000;
 	public static int MIN_PERIOD = 1;
 	public static int MAX_PERIOD = 1000;
 	public static int TOTAL_PARTITIONS = 16;
@@ -40,34 +32,19 @@ public class ProtocolsCombinedWFDMSuccessF {
 	public static boolean useRi = true;
 	public static boolean btbHit = true;
 
-	public static int GENERATIONS = 100;
-	public static int POPULATION = 100;
+	public static int GENERATIONS = 5;
+	public static int POPULATION = 5;
 	public static int ALLOCATION_POLICY = 1;
 	public static int PRIORITY_RULE = 1;
 
 	public static boolean useGA = true;
 	public static boolean lazy = true;
 	public static boolean record = false;
-	
 
 	class Counter {
-		int fnp = 0;
-		int fp = 0;
-		int mrsp = 0;
+		int count = 0;
 		int Dcombine = 0;
 		int Dnew = 0;
-
-		public synchronized void incfnp() {
-			fnp++;
-		}
-
-		public synchronized void incfp() {
-			fp++;
-		}
-
-		public synchronized void incmrsp() {
-			mrsp++;
-		}
 
 		public synchronized void incDcombine() {
 			Dcombine++;
@@ -77,12 +54,14 @@ public class ProtocolsCombinedWFDMSuccessF {
 			Dnew++;
 		}
 
+		public synchronized void incCount() {
+			count++;
+		}
+
 		public synchronized void initResults() {
-			mrsp = 0;
-			fp = 0;
-			fnp = 0;
 			Dcombine = 0;
 			Dnew = 0;
+			count = 0;
 		}
 	}
 
@@ -119,8 +98,7 @@ public class ProtocolsCombinedWFDMSuccessF {
 		}
 
 		downLatch.await();
-		
-		ResultReader.readsuccessF("result", bigTest, smallTest );
+
 		System.out.println("FINISHED!!!");
 	}
 
@@ -150,240 +128,114 @@ public class ProtocolsCombinedWFDMSuccessF {
 			break;
 		}
 
-		final CountDownLatch timesdownLatch = new CountDownLatch(NUMBER_OF_TIMES);
-		for (int j = 0; j < NUMBER_OF_TIMES; j++) {
-			final int fatherindex = j;
-			new Thread(new Runnable() {
+		CountDownLatch numbersdownLatch = new CountDownLatch(TOTAL_NUMBER_OF_SYSTEMS);
+		for (int i = 0; i < TOTAL_NUMBER_OF_SYSTEMS; i++) {
+			final int childindex = i;
+			Thread worker = new Thread(new Runnable() {
 
 				@Override
 				public void run() {
+					SystemGenerator generator = new SystemGenerator(MIN_PERIOD, MAX_PERIOD, true, TOTAL_PARTITIONS,
+							TOTAL_PARTITIONS * NUMBER_OF_TASKS_ON_EACH_PARTITION, RSF, cs_range, RESOURCES_RANGE.PARTITIONS,
+							NUMBER_OF_MAX_ACCESS_TO_ONE_RESOURCE, false);
 
-					final Counter subcount = new Counter();
+					ArrayList<SporadicTask> tasksToAlloc = generator.generateTasks();
+					ArrayList<Resource> resources = generator.generateResources();
+					generator.generateResourceUsage(tasksToAlloc, resources);
+					PreGASolver pre = new PreGASolver(tasksToAlloc, resources, generator, 3, 1, 1, false);
 
-					final CountDownLatch numbersdownLatch = new CountDownLatch(TOTAL_NUMBER_OF_SYSTEMS);
-					for (int i = 0; i < TOTAL_NUMBER_OF_SYSTEMS; i++) {
-						final int childindex = i;
-						Thread worker = new Thread(new Runnable() {
+					int preres = pre.initialCheck(true);
+					while (preres == 1 || preres == -1) {
+						tasksToAlloc = generator.generateTasks();
+						resources = generator.generateResources();
+						generator.generateResourceUsage(tasksToAlloc, resources);
 
-							public boolean isSystemSchedulable(ArrayList<ArrayList<SporadicTask>> tasks, long[][] Ris) {
-								for (int i = 0; i < tasks.size(); i++) {
-									for (int j = 0; j < tasks.get(i).size(); j++) {
-										if (tasks.get(i).get(j).deadline < Ris[i][j])
-											return false;
-									}
-								}
-								return true;
-							}
-
-							@Override
-							public void run() {
-								SystemGenerator generator = new SystemGenerator(MIN_PERIOD, MAX_PERIOD, true, TOTAL_PARTITIONS,
-										TOTAL_PARTITIONS * NUMBER_OF_TASKS_ON_EACH_PARTITION, RSF, cs_range, RESOURCES_RANGE.PARTITIONS,
-										NUMBER_OF_MAX_ACCESS_TO_ONE_RESOURCE, false);
-
-								ArrayList<SporadicTask> tasksToAlloc = generator.generateTasks();
-								ArrayList<Resource> resources = generator.generateResources();
-								generator.generateResourceUsage(tasksToAlloc, resources);
-								PreGASolver pre = new PreGASolver(tasksToAlloc, resources, generator, 3, 1, 1, false);
-
-								int preres = pre.initialCheck(true);
-								while (preres == 1) {
-									tasksToAlloc = generator.generateTasks();
-									resources = generator.generateResources();
-									generator.generateResourceUsage(tasksToAlloc, resources);
-
-									pre = new PreGASolver(tasksToAlloc, resources, generator, 3, 1, 1, false);
-									preres = pre.initialCheck(true);
-								}
-
-								ArrayList<ArrayList<SporadicTask>> tasks = new AllocationGeneator().allocateTasks(tasksToAlloc, resources,
-										generator.total_partitions, 0);
-
-								long[][] Ris;
-								MrsP mrsp = new MrsP();
-								FIFOP fp = new FIFOP();
-								FIFONP fnp = new FIFONP();
-
-								GASolver solver = new GASolver(tasksToAlloc, resources, generator, ALLOCATION_POLICY, PRIORITY_RULE, POPULATION, GENERATIONS, 2,
-										2, 0.8, 0.01, 2, 2, record, true);
-								solver.name = "GA: " + Thread.currentThread().getName();
-
-								Ris = mrsp.getResponseTimeByDMPO(tasks, resources, AnalysisUtils.extendCalForStatic, true, btbHit, useRi, false);
-								if (isSystemSchedulable(tasks, Ris)) {
-									counter.incmrsp();
-									subcount.incmrsp();
-								}
-
-								Ris = fnp.getResponseTimeByDMPO(tasks, resources, AnalysisUtils.extendCalForStatic, true, btbHit, useRi, false);
-								if (isSystemSchedulable(tasks, Ris)) {
-									counter.incfnp();
-									subcount.incfnp();
-								}
-
-								Ris = fp.getResponseTimeByDMPO(tasks, resources, AnalysisUtils.extendCalForStatic, true, btbHit, useRi, false);
-								if (isSystemSchedulable(tasks, Ris)) {
-									counter.incfp();
-									subcount.incfp();
-								}
-
-								if (solver.checkSchedulability(useGA, lazy) == 1) {
-									counter.incDcombine();
-									subcount.incDcombine();
-									if (solver.bestProtocol == 0) {
-										counter.incDnew();
-										subcount.incDnew();
-									}
-								}
-
-								System.out.println(Thread.currentThread().getName() + " Finish.");
-								numbersdownLatch.countDown();
-							}
-						});
-						worker.setName("2 " + cslen + " times: " + fatherindex + " numbers: " + childindex);
-						worker.start();
+						pre = new PreGASolver(tasksToAlloc, resources, generator, 3, 1, 1, false);
+						preres = pre.initialCheck(true);
 					}
 
-					try {
-						numbersdownLatch.await();
-					} catch (InterruptedException e) {
+					GASolver solver = new GASolver(tasksToAlloc, resources, generator, ALLOCATION_POLICY, PRIORITY_RULE, POPULATION, GENERATIONS, 2, 2, 0.8,
+							0.01, 2, 2, record, true);
+					solver.name = "GA: " + Thread.currentThread().getName();
+
+					if (solver.checkSchedulability(useGA, lazy) == 1) {
+						counter.incDcombine();
+						if (solver.bestProtocol == 0) {
+							counter.incDnew();
+						}
 					}
 
-					String subresult = cslen + "|" + fatherindex + " " + subcount.fnp + " " + subcount.fp + " " + subcount.mrsp + " " + subcount.Dcombine + " "
-							+ subcount.Dnew;
-					writeSystem("2 2 " + cslen + " " + fatherindex, subresult);
-
-					timesdownLatch.countDown();
-
+					counter.incCount();
+					System.out.println(Thread.currentThread().getName() + " Finish. count: " + counter.count);
+					numbersdownLatch.countDown();
 				}
-			}).start();
-
+			});
+			worker.setName("2 " + cslen + " numbers: " + childindex);
+			worker.start();
 		}
 
 		try {
-			timesdownLatch.await();
+			numbersdownLatch.await();
 		} catch (InterruptedException e) {
 		}
 
-		String result = "cslen-" + cslen + " " + counter.fnp + " " + counter.fp + " " + counter.mrsp + " " + counter.Dcombine + " " + counter.Dnew;
+		String result = "cslen-" + cslen + " " + counter.Dcombine + " " + counter.Dnew;
 		writeSystem("2 2 " + cslen, result);
 
 	}
 
 	public void parallelExperimentIncreasingAccess(int NoA, Counter counter) {
-		final CountDownLatch timesdownLatch = new CountDownLatch(NUMBER_OF_TIMES);
-		for (int j = 0; j < NUMBER_OF_TIMES; j++) {
-			final int fatherindex = j;
-			new Thread(new Runnable() {
+		CountDownLatch numbersdownLatch = new CountDownLatch(TOTAL_NUMBER_OF_SYSTEMS);
+		for (int i = 0; i < TOTAL_NUMBER_OF_SYSTEMS; i++) {
+			final int childindex = i;
+			Thread worker = new Thread(new Runnable() {
 
 				@Override
 				public void run() {
+					SystemGenerator generator = new SystemGenerator(MIN_PERIOD, MAX_PERIOD, true, TOTAL_PARTITIONS,
+							TOTAL_PARTITIONS * NUMBER_OF_TASKS_ON_EACH_PARTITION, RSF, range, RESOURCES_RANGE.PARTITIONS, NoA, false);
 
-					final Counter subcount = new Counter();
+					ArrayList<SporadicTask> tasksToAlloc = generator.generateTasks();
+					ArrayList<Resource> resources = generator.generateResources();
+					generator.generateResourceUsage(tasksToAlloc, resources);
+					PreGASolver pre = new PreGASolver(tasksToAlloc, resources, generator, 3, 1, 1, false);
 
-					final CountDownLatch numbersdownLatch = new CountDownLatch(TOTAL_NUMBER_OF_SYSTEMS);
-					for (int i = 0; i < TOTAL_NUMBER_OF_SYSTEMS; i++) {
-						final int childindex = i;
-						Thread worker = new Thread(new Runnable() {
+					int preres = pre.initialCheck(true);
+					while (preres == 1 || preres == -1) {
+						tasksToAlloc = generator.generateTasks();
+						resources = generator.generateResources();
+						generator.generateResourceUsage(tasksToAlloc, resources);
 
-							public boolean isSystemSchedulable(ArrayList<ArrayList<SporadicTask>> tasks, long[][] Ris) {
-								for (int i = 0; i < tasks.size(); i++) {
-									for (int j = 0; j < tasks.get(i).size(); j++) {
-										if (tasks.get(i).get(j).deadline < Ris[i][j])
-											return false;
-									}
-								}
-								return true;
-							}
-
-							@Override
-							public void run() {
-								SystemGenerator generator = new SystemGenerator(MIN_PERIOD, MAX_PERIOD, true, TOTAL_PARTITIONS,
-										TOTAL_PARTITIONS * NUMBER_OF_TASKS_ON_EACH_PARTITION, RSF, range, RESOURCES_RANGE.PARTITIONS, NoA, false);
-
-								ArrayList<SporadicTask> tasksToAlloc = generator.generateTasks();
-								ArrayList<Resource> resources = generator.generateResources();
-								generator.generateResourceUsage(tasksToAlloc, resources);
-								PreGASolver pre = new PreGASolver(tasksToAlloc, resources, generator, 3, 1, 1, false);
-
-								int preres = pre.initialCheck(true);
-								while (preres == 1) {
-									tasksToAlloc = generator.generateTasks();
-									resources = generator.generateResources();
-									generator.generateResourceUsage(tasksToAlloc, resources);
-
-									pre = new PreGASolver(tasksToAlloc, resources, generator, 3, 1, 1, false);
-									preres = pre.initialCheck(true);
-								}
-
-								ArrayList<ArrayList<SporadicTask>> tasks = new AllocationGeneator().allocateTasks(tasksToAlloc, resources,
-										generator.total_partitions, 0);
-
-								long[][] Ris;
-								MrsP mrsp = new MrsP();
-								FIFOP fp = new FIFOP();
-								FIFONP fnp = new FIFONP();
-
-								GASolver solver = new GASolver(tasksToAlloc, resources, generator, ALLOCATION_POLICY, PRIORITY_RULE, POPULATION, GENERATIONS, 2,
-										2, 0.8, 0.01, 2, 2, record, true);
-								solver.name = "GA: " + Thread.currentThread().getName();
-
-								Ris = mrsp.getResponseTimeByDMPO(tasks, resources, AnalysisUtils.extendCalForStatic, true, btbHit, useRi, false);
-								if (isSystemSchedulable(tasks, Ris)) {
-									counter.incmrsp();
-									subcount.incmrsp();
-								}
-
-								Ris = fnp.getResponseTimeByDMPO(tasks, resources, AnalysisUtils.extendCalForStatic, true, btbHit, useRi, false);
-								if (isSystemSchedulable(tasks, Ris)) {
-									counter.incfnp();
-									subcount.incfnp();
-								}
-
-								Ris = fp.getResponseTimeByDMPO(tasks, resources, AnalysisUtils.extendCalForStatic, true, btbHit, useRi, false);
-								if (isSystemSchedulable(tasks, Ris)) {
-									counter.incfp();
-									subcount.incfp();
-								}
-
-								if (solver.checkSchedulability(useGA, lazy) == 1) {
-									counter.incDcombine();
-									subcount.incDcombine();
-									if (solver.bestProtocol == 0) {
-										counter.incDnew();
-										subcount.incDnew();
-									}
-								}
-
-								System.out.println(Thread.currentThread().getName() + " Finish.");
-								numbersdownLatch.countDown();
-							}
-						});
-						worker.setName("3 " + NoA + " times: " + fatherindex + " numbers: " + childindex);
-						worker.start();
+						pre = new PreGASolver(tasksToAlloc, resources, generator, 3, 1, 1, false);
+						preres = pre.initialCheck(true);
 					}
 
-					try {
-						numbersdownLatch.await();
-					} catch (InterruptedException e) {
+					GASolver solver = new GASolver(tasksToAlloc, resources, generator, ALLOCATION_POLICY, PRIORITY_RULE, POPULATION, GENERATIONS, 2, 2, 0.8,
+							0.01, 2, 2, record, true);
+					solver.name = "GA: " + Thread.currentThread().getName();
+
+					if (solver.checkSchedulability(useGA, lazy) == 1) {
+						counter.incDcombine();
+						if (solver.bestProtocol == 0) {
+							counter.incDnew();
+						}
 					}
 
-					String subresult = NoA + "|" + fatherindex + " " + subcount.fnp + " " + subcount.fp + " " + subcount.mrsp + " " + subcount.Dcombine + " "
-							+ subcount.Dnew;
-					writeSystem("3 2 " + NoA + " " + fatherindex, subresult);
-
-					timesdownLatch.countDown();
-
+					counter.incCount();
+					System.out.println(Thread.currentThread().getName() + " Finish. count: " + counter.count);
+					numbersdownLatch.countDown();
 				}
-			}).start();
-
+			});
+			worker.setName("3 " + NoA + " numbers: " + childindex);
+			worker.start();
 		}
 
 		try {
-			timesdownLatch.await();
+			numbersdownLatch.await();
 		} catch (InterruptedException e) {
 		}
 
-		String result = "access-" + NoA + " " + counter.fnp + " " + counter.fp + " " + counter.mrsp + " " + counter.Dcombine + " " + counter.Dnew;
+		String result = "access-" + NoA + " " + counter.Dcombine + " " + counter.Dnew;
 		writeSystem("3 2 " + NoA, result);
 	}
 
